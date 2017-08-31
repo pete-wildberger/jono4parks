a34cc6a4b84ba749e6e5fff321a494d0ee3c6483
Peter Wildberger <pete.wildberger@gmail.com>
2017-08-31 10:21:08 -0500

    dotenv

--- config/database.js
+++ config/database.js
@@ -1,8 +1,3 @@
-/**
- * Connection URI for a Mongo database that will hold our
- * application's persistent data.
- *
- * @todo Update to match the path of your actual database.
- * @module config/database
- */
-module.exports = 'mongodb://localhost:27017/jono4parks';
+
+// module.exports = 'mongodb://localhost:27017/jono4parks';
+module.exports = process.env.MONGODB_URI;
