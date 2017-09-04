55aa743f809382d90db943022d65a511c564572a
Peter Wildberger <pete.wildberger@gmail.com>
2017-09-04 14:28:22 -0500

    sunday work

--- public/scripts/controllers/IndexController.js
+++ public/scripts/controllers/IndexController.js
@@ -7,5 +7,8 @@ function IndexController($location, httpService, AuthFactory) {
   console.log('IndexController');
   const vm = this;
 
-
+  vm.go = function (path) {
+    console.log('donate');
+    $location.path(path);
+  };
 }
