7cff76779c6d7fab57868f1191d56a951e429a34
Peter Wildberger <pete.wildberger@gmail.com>
2017-09-13 09:01:52 -0500

    no more #!

--- public/scripts/ngconfig.js
+++ public/scripts/ngconfig.js
@@ -1,38 +1,48 @@
-angular.module('myApp')
-  .config(router);
-  router.$inject= ['$routeProvider'];
-function router($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl: "/views/partials/welcome.html",
-        controller: "IndexController as ic",
-        controllerAs:"ic"
-    }).when('/endorsements', {
-        templateUrl: "/views/partials/endorse.html",
-        controller: "EndorseController as ec",
-        controllerAs:"ec"
-    }).when('/dash', {
-        templateUrl: "/views/partials/dashboard.html",
-        controller: "DashController as dc",
-        controllerAs:"dc"
-    }).when('/admin', {
-        templateUrl: "/views/partials/admin.html",
-        controller: "IndexController as ic",
-        controllerAs:"ic"
-    }).when('/issues', {
-        templateUrl: "/views/partials/issues.html",
-        controller: "IndexController as ic",
-        controllerAs:"ic"
-    }).when('/about', {
-        templateUrl: "/views/partials/about.html",
-        controller: "IndexController as ic",
-        controllerAs:"ic"
-    }).when('/support', {
-        templateUrl: "/views/partials/support.html",
-        controller: "SupportController as sc",
-        controllerAs:"sc"
-    }).when('/events', {
-        templateUrl: "/views/partials/events.html",
-        controller: "EventsController as ec",
-        controllerAs:"ec"
-    });
-  }
+angular.module('myApp').config(router);
+router.$inject = ['$routeProvider', '$locationProvider'];
+function router($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/views/partials/welcome.html',
+      controller: 'IndexController as ic',
+      controllerAs: 'ic'
+    })
+    .when('/endorsements', {
+      templateUrl: '/views/partials/endorse.html',
+      controller: 'EndorseController as ec',
+      controllerAs: 'ec'
+    })
+    .when('/dash', {
+      templateUrl: '/views/partials/dashboard.html',
+      controller: 'DashController as dc',
+      controllerAs: 'dc'
+    })
+    .when('/admin', {
+      templateUrl: '/views/partials/admin.html',
+      controller: 'IndexController as ic',
+      controllerAs: 'ic'
+    })
+    .when('/issues', {
+      templateUrl: '/views/partials/issues.html',
+      controller: 'IndexController as ic',
+      controllerAs: 'ic'
+    })
+    .when('/about', {
+      templateUrl: '/views/partials/about.html',
+      controller: 'IndexController as ic',
+      controllerAs: 'ic'
+    })
+    .when('/support', {
+      templateUrl: '/views/partials/support.html',
+      controller: 'SupportController as sc',
+      controllerAs: 'sc'
+    })
+    .when('/events', {
+      templateUrl: '/views/partials/events.html',
+      controller: 'EventsController as ec',
+      controllerAs: 'ec'
+    })
+    .otherwise({ redirectTo: '/' });
+
+  $locationProvider.html5Mode(true);
+}
--- server.js
+++ server.js
@@ -3,32 +3,36 @@ require('dotenv').config({
 });
 // requires etc
 const express = require('express'),
-app = express(),
-path = require('path'),
-bodyParser = require('body-parser'),
-port = process.env.PORT || 4000,
-session = require('express-session'),
-passport = require('./auth/passport'),
-index = require('./routes/index'),
-auth = require('./routes/auth'),
-isLoggedIn = require('./utils/auth'),
-private = require('./routes/private/index'),
-database = require('./utils/database');
+  app = express(),
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  port = process.env.PORT || 4000,
+  session = require('express-session'),
+  passport = require('./auth/passport'),
+  index = require('./routes/index'),
+  auth = require('./routes/auth'),
+  isLoggedIn = require('./utils/auth'),
+  private = require('./routes/private/index'),
+  database = require('./utils/database');
 
 database();
 // uses
-app.use(express.static ('public'));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(express.static('public'));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(bodyParser.json());
-app.use(session({
-  secret: process.env.SECRET,
-  key: 'user',
-  resave: 'true',
-  saveUninitialized: false,
-  cookie: { maxage: 60000, secure: false },
-}));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    key: 'user',
+    resave: 'true',
+    saveUninitialized: false,
+    cookie: { maxage: 60000, secure: false }
+  })
+);
 app.use(passport.initialize()); // passport
 app.use(passport.session());
 
@@ -37,6 +41,12 @@ app.use('/auth', auth);
 app.use('/private', isLoggedIn, private);
 app.use('/', index);
 
-app.listen(port, function(){
+app.listen(port, function() {
   console.log('server up on 4000');
+});
+
+app.use(function(req, res) {
+  res.sendFile('index.html', {
+    root: 'public/views/'
+  });
 });
