da687b53694c6414ccaa2ea48bad6f647317dccf
Peter Wildberger <pete.wildberger@gmail.com>
2017-09-14 08:40:07 -0500

    sierra add

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,22 @@
 const express = require('express'),
-router = express.Router(),
-path = require('path'),
-endorse = require('./endorse.js'),
-messages = require('./messages.js'),
-events = require('./events.js'),
-signs = require('./signs.js');
+  router = express.Router(),
+  path = require('path'),
+  endorse = require('./endorse.js'),
+  messages = require('./messages.js'),
+  events = require('./events.js'),
+  signs = require('./signs.js');
 
 router.use('/endorse', endorse);
 router.use('/messages', messages);
 router.use('/events', events);
 router.use('/signs', signs);
 
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
   res.sendFile(path.resolve('public/views/index.html'));
+});
+
+router.get('/sierra', function(req, res) {
+  res.sendFile(path.resolve('public/views/j4pSierraClubQuestionnaire.pdf'));
 });
 
 module.exports = router;
